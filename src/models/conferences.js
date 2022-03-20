3778777ed53ca733f53b2f3f0d060817e6c9cbe4
khanos <epilef.rodriguez@gmail.com>
2022-03-20 08:33:22 -0400

    added a new endpoint to filter by conference author, added integration test

--- src/models/conferences.js
+++ src/models/conferences.js
@@ -1,7 +1,12 @@
+const { v4: uuidv4 } = require('uuid');
 const conference = [];
 module.exports = {
   addData: (data) => {
+    data.id = uuidv4();
     return conference.push(data);
+  },
+  addBulkData: (data) => {
+    return conference.push(...data);
   },
   getData: () => {
     return conference;
